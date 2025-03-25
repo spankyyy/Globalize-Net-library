18451d107160817d23b8a594a40523f9c5e690d8
spankyyy <exoticbeats6969@gmail.com>
2025-03-25 07:53:56 -0400

    Localize functions and tables
    Made Globalize.Internal not accessible from the outside by making it a local table
    Localized every other global functions

--- lua/autorun/!globalize.lua
+++ lua/autorun/!globalize.lua
@@ -12,21 +12,50 @@ end
 
 --------------------------------------------------------------------------------------------------------------------------------
 
-Fallback(_G, "Globalize", {}) -- Globalize
-Fallback(Globalize, "Internal", {}) -- Globalize.Internal
-Fallback(Globalize.Internal, "Receivers", {}) -- Globalize.Internal.Receivers
-Fallback(Globalize.Internal, "RateLimiting", {}) -- Globalize.Internal.RateLimiting
-Fallback(Globalize.Internal, "GlobalVariables", {}) -- Globalize.Internal.GlobalVariables
+local net_Start = net.Start
+local net_WriteUInt = net.WriteUInt
+local net_WriteData = net.WriteData
+local net_WriteBool = net.WriteBool
+local net_ReadUInt = net.ReadUInt
+local net_ReadData = net.ReadData
+local net_ReadBool = net.ReadBool
+local net_Receive = net.Receive
+local net_SendToServer = net.SendToServer
+local net_Send = net.Send
+local net_Broadcast = net.Broadcast
+local timer_Create = timer.Create
+local hook_Add = hook.Add
+local hook_Run = hook.Run
+local math_random = math.random
+local math_ceil = math.ceil
+local util_Compress = util.Compress
+local util_Decompress = util.Decompress
+local util_JSONToTable = util.JSONToTable
+local util_TableToJSON = util.TableToJSON
+local util_AddNetworkString = util.AddNetworkString
+
+local CurTime = CurTime
+local MsgC = MsgC
+local IsValid = IsValid
 
 --------------------------------------------------------------------------------------------------------------------------------
 
-Globalize.Internal.MaxPacketSize = 2^15
-Globalize.Internal.PacketSegmentDelay = 0.1
-Globalize.Internal.RequestID = 0 -- Increments everytime we send a packet
+local GlobalizeInternal = {}
+
+Fallback(_G, "Globalize", {}) -- Globalize
+Fallback(GlobalizeInternal, "Receivers", {}) -- GlobalizeInternal.Receivers
+Fallback(GlobalizeInternal, "RateLimiting", {}) -- GlobalizeInternal.RateLimiting
+Fallback(GlobalizeInternal, "GlobalVariables", {}) -- GlobalizeInternal.GlobalVariables
+
+--------------------------------------------------------------------------------------------------------------------------------
+
+GlobalizeInternal.MaxPacketSize = 2^15
+GlobalizeInternal.PacketSegmentDelay = 0.1
+GlobalizeInternal.RequestID = 0 -- Increments everytime we send a packet
 
 local function bufferSplitter(buffer, maxBufferSize)
     local bufferLen = #buffer
-    local bufferSplits = math.ceil(bufferLen / maxBufferSize)
+    local bufferSplits = math_ceil(bufferLen / maxBufferSize)
     local buffers = {}
     for i = 1, bufferSplits do
         local start = (i - 1) * maxBufferSize + 1
@@ -36,11 +65,11 @@ local function bufferSplitter(buffer, maxBufferSize)
     return buffers
 end
 
-function Globalize.Internal.SplitPacket(Packet, MaxSize)
+function GlobalizeInternal.SplitPacket(Packet, MaxSize)
     local Size = #Packet
     if Size < MaxSize then return {}, 0 end
 
-    local Splits = math.ceil(Size / MaxSize)
+    local Splits = math_ceil(Size / MaxSize)
     local SegmentedPackets = {}
 
     for i=1, Splits do
@@ -51,7 +80,7 @@ function Globalize.Internal.SplitPacket(Packet, MaxSize)
     return SegmentedPackets, Splits
 end
 
-function Globalize.Internal.Message(...)
+function GlobalizeInternal.Message(...)
     MsgC(Color(255, 128, 0), "[Globalize]", Color(235, 235, 235), ": ", unpack({...}), "\n")
 end
 
@@ -59,33 +88,33 @@ end
 
 local function WriteData(Data)
     local Size = #Data
-    net.WriteUInt(#Data, 16)
-    net.WriteData(Data, Size)
+    net_WriteUInt(#Data, 16)
+    net_WriteData(Data, Size)
 end
 
 local function ReadData()
-    local Size = net.ReadUInt(16)
-    return net.ReadData(Size)
+    local Size = net_ReadUInt(16)
+    return net_ReadData(Size)
 end
 
 local function Send(Recipients)
     if CLIENT then
-        net.SendToServer()
+        net_SendToServer()
         return
     end
 
     local Type = type(Recipients)
     if Recipients and Type == "Player" or Type == "table" then 
-        net.Send(Recipients)
+        net_Send(Recipients)
         return
     end
-    net.Broadcast()
+    net_Broadcast()
 end
 
 --------------------------------------------------------------------------------------------------------------------------------
 
 local function UncompressPacket(CompressedPacket)
-    return util.JSONToTable(util.Decompress(CompressedPacket))
+    return util_JSONToTable(util_Decompress(CompressedPacket))
 end
 
 local function CreateCompressedPacket(NetworkID, Data)
@@ -94,13 +123,13 @@ local function CreateCompressedPacket(NetworkID, Data)
         data = Data
     }
 
-    return util.Compress(util.TableToJSON(Packet))
+    return util_Compress(util_TableToJSON(Packet))
 end
 
 --------------------------------------------------------------------------------------------------------------------------------
 
 if SERVER then
-    util.AddNetworkString("Globalize.NetworkChannel")
+    util_AddNetworkString("Globalize.NetworkChannel")
 
     function Globalize.LimitRate(NetworkID, Ply, Delay)
         if not NetworkID then return false end
@@ -113,16 +142,14 @@ if SERVER then
         local CurrentTime = CurTime()
         local PlayerID = Ply:SteamID64()
 
-        local Internal = Globalize.Internal
+        Fallback(GlobalizeInternal.RateLimiting, PlayerID, {})
+        Fallback(GlobalizeInternal.RateLimiting[PlayerID], NetworkID, 0)
 
-        Fallback(Internal.RateLimiting, PlayerID, {})
-        Fallback(Internal.RateLimiting[PlayerID], NetworkID, 0)
-
-        if Internal.RateLimiting[PlayerID][NetworkID] > CurrentTime then
+        if GlobalizeInternal.RateLimiting[PlayerID][NetworkID] > CurrentTime then
             IsLimited = true
         end
 
-        Internal.RateLimiting[PlayerID][NetworkID] = CurrentTime + Delay
+        GlobalizeInternal.RateLimiting[PlayerID][NetworkID] = CurrentTime + Delay
 
         return IsLimited
     end
@@ -131,64 +158,62 @@ if SERVER then
         if not NetworkID then return end
         if not Data then Data = {} end
 
-        local Internal = Globalize.Internal
-
-        Internal.RequestID = Internal.RequestID + 1 -- Used for differencing packets from the same NetworkID but not from the same unsegmented packet
+        GlobalizeInternal.RequestID = GlobalizeInternal.RequestID + 1 -- Used for differencing packets from the same NetworkID but not from the same unsegmented packet
 
         local CompressedPacket = CreateCompressedPacket(NetworkID, Data)
-        local Segments, NumSegments = Internal.SplitPacket(CompressedPacket, Internal.MaxPacketSize)
+        local Segments, NumSegments = GlobalizeInternal.SplitPacket(CompressedPacket, GlobalizeInternal.MaxPacketSize)
 
         if NumSegments == 0 then
-            net.Start("Globalize.NetworkChannel")
+            net_Start("Globalize.NetworkChannel")
 
-            net.WriteBool(false) -- Packet segmented?
+            net_WriteBool(false) -- Packet segmented?
             WriteData(CompressedPacket) -- Data
     
             Send(Recipients)
             return
         end
 
-        Globalize.Internal.Message(
+        GlobalizeInternal.Message(
             string.format("Packet too BIG (%i bytes) max(%i bytes), Segmenting packet (%i Segments) (%i bytes max) (eta %G seconds)", 
             #CompressedPacket, 
-            Internal.MaxPacketSize, 
+            GlobalizeInternal.MaxPacketSize, 
             NumSegments, 
-            Internal.MaxPacketSize,
-            NumSegments * Internal.PacketSegmentDelay
+            GlobalizeInternal.MaxPacketSize,
+            NumSegments * GlobalizeInternal.PacketSegmentDelay
         ))
 
         local SegmentsSent = 0
-        local SegmentID = Internal.RequestID
+        local SegmentID = GlobalizeInternal.RequestID
 
-        timer.Create(NetworkID .. math.random(-9e9, 9e9), Internal.PacketSegmentDelay, NumSegments, function()
+        timer_Create(NetworkID .. math_random(-9e9, 9e9), GlobalizeInternal.PacketSegmentDelay, NumSegments, function()
             SegmentsSent = SegmentsSent + 1
             local Segment = Segments[SegmentsSent]
 
-            net.Start("Globalize.NetworkChannel")
+            net_Start("Globalize.NetworkChannel")
 
-            net.WriteBool(true) -- Packet segmented?
-            net.WriteUInt(SegmentID, 16) -- Segment ID
-            net.WriteUInt(SegmentsSent, 16) -- Segments sent
-            net.WriteUInt(NumSegments, 16) -- Number of segments
+            net_WriteBool(true) -- Packet segmented?
+            net_WriteUInt(SegmentID, 16) -- Segment ID
+            net_WriteUInt(SegmentsSent, 16) -- Segments sent
+            net_WriteUInt(NumSegments, 16) -- Number of segments
             WriteData(Segment) -- Data 
 
             Send(Recipients)
         end)
     end
 
-    function Globalize.Internal.CallReceiver(NetworkID, Data, Len, Ply)
+    function GlobalizeInternal.CallReceiver(NetworkID, Data, Len, Ply)
         if not NetworkID then return end
         if not Data then return end
-        if not Globalize.Internal.Receivers[NetworkID] then return end
+        if not GlobalizeInternal.Receivers[NetworkID] then return end
         if not IsValid(Ply) then return end
 
-        local _Receiver = Globalize.Internal.Receivers[NetworkID]
+        local _Receiver = GlobalizeInternal.Receivers[NetworkID]
 
         local Callback = _Receiver.callback
         local Delay = _Receiver.ratelimit
 
         if Globalize.LimitRate(NetworkID, Ply, Delay) then
-            Globalize.Internal.Message(string.format("[%s|%i|%s] hit ratelimit! [%s] only accepts packets every %G seconds", 
+            GlobalizeInternal.Message(string.format("[%s|%i|%s] hit ratelimit! [%s] only accepts packets every %G seconds", 
                 Ply:Name(), 
                 Ply:EntIndex(), 
                 Ply:SteamID(), 
@@ -209,56 +234,54 @@ if CLIENT then
         if not NetworkID then return end
         if not Data then Data = {} end
 
-        local Internal = Globalize.Internal
-
-        Internal.RequestID = Internal.RequestID + 1 -- Used for differencing packets from the same NetworkID but not from the same unsegmented packet
+        GlobalizeInternal.RequestID = GlobalizeInternal.RequestID + 1 -- Used for differencing packets from the same NetworkID but not from the same unsegmented packet
 
         local CompressedPacket = CreateCompressedPacket(NetworkID, Data)
-        local Segments, NumSegments = Internal.SplitPacket(CompressedPacket, Internal.MaxPacketSize)
+        local Segments, NumSegments = GlobalizeInternal.SplitPacket(CompressedPacket, GlobalizeInternal.MaxPacketSize)
 
         if NumSegments == 0 then
-            net.Start("Globalize.NetworkChannel")
+            net_Start("Globalize.NetworkChannel")
 
-            net.WriteBool(false) -- Packet segmented?
+            net_WriteBool(false) -- Packet segmented?
             WriteData(CompressedPacket) -- Data
     
             Send()
         end
 
-        Globalize.Internal.Message(
+        GlobalizeInternal.Message(
             string.format("Packet too BIG (%i bytes) max(%i bytes), Segmenting packet (%i Segments) (%i bytes max) (eta %G seconds)", 
             #CompressedPacket, 
-            Internal.MaxPacketSize, 
+            GlobalizeInternal.MaxPacketSize, 
             NumSegments, 
-            Internal.MaxPacketSize,
-            NumSegments * Internal.PacketSegmentDelay
+            GlobalizeInternal.MaxPacketSize,
+            NumSegments * GlobalizeInternal.PacketSegmentDelay
         ))
 
         local SegmentsSent = 0
-        local SegmentID = Internal.RequestID
+        local SegmentID = GlobalizeInternal.RequestID
 
-        timer.Create(NetworkID .. math.random(-9e9, 9e9), Internal.PacketSegmentDelay, NumSegments, function()
+        timer_Create(NetworkID .. math_random(-9e9, 9e9), GlobalizeInternal.PacketSegmentDelay, NumSegments, function()
             SegmentsSent = SegmentsSent + 1
             local Segment = Segments[SegmentsSent]
 
-            net.Start("Globalize.NetworkChannel")
+            net_Start("Globalize.NetworkChannel")
 
-            net.WriteBool(true) -- Packet segmented?
-            net.WriteUInt(SegmentID, 16) -- Segment ID
-            net.WriteUInt(SegmentsSent, 16) -- Segments sent
-            net.WriteUInt(NumSegments, 16) -- Number of segments
+            net_WriteBool(true) -- Packet segmented?
+            net_WriteUInt(SegmentID, 16) -- Segment ID
+            net_WriteUInt(SegmentsSent, 16) -- Segments sent
+            net_WriteUInt(NumSegments, 16) -- Number of segments
             WriteData(Segment) -- Data 
 
             Send()
         end)
     end
 
-    function Globalize.Internal.CallReceiver(NetworkID, Data, Len, Ply)
+    function GlobalizeInternal.CallReceiver(NetworkID, Data, Len, Ply)
         if not NetworkID then return end
         if not Data then return end
-        if not Globalize.Internal.Receivers[NetworkID] then return end
+        if not GlobalizeInternal.Receivers[NetworkID] then return end
     
-        local _Receiver = Globalize.Internal.Receivers[NetworkID]
+        local _Receiver = GlobalizeInternal.Receivers[NetworkID]
 
         local Callback = _Receiver.callback
 
@@ -272,11 +295,11 @@ function Globalize.Subscribe(NetworkID, Callback, RateLimit)
     if not NetworkID then return end
     if not Callback then return end
 
-    local Internal = Globalize.Internal
+    
 
-    Fallback(Internal.Receivers, NetworkID, {})
+    Fallback(GlobalizeInternal.Receivers, NetworkID, {})
 
-    Internal.Receivers[NetworkID] = {
+    GlobalizeInternal.Receivers[NetworkID] = {
         callback = Callback,
         ratelimit = RateLimit or 0
     }
@@ -285,27 +308,28 @@ end
 function Globalize.Unsubscribe(NetworkID)
     if not NetworkID then return end
 
-    local Internal = Globalize.Internal
+    
 
-    Fallback(Internal.Receivers, NetworkID, {})
+    Fallback(GlobalizeInternal.Receivers, NetworkID, {})
 
-    Internal.Receivers[NetworkID] = nil
+    GlobalizeInternal.Receivers[NetworkID] = nil
 end
 
 --------------------------------------------------------------------------------------------------------------------------------
 
-Globalize.Internal.ActiveSegmentedPackets = {}
-net.Receive("Globalize.NetworkChannel", function(len, ply)
-    local IsSegmented = net.ReadBool()
+GlobalizeInternal.ActiveSegmentedPackets = {}
+net_Receive("Globalize.NetworkChannel", function(len, ply)
+    local IsSegmented = net_ReadBool()
+    
     
     if IsSegmented then
         
-        local SegmentID = net.ReadUInt(16)
-        local SegmentsSent = net.ReadUInt(16)
-        local NumSegments = net.ReadUInt(16)
+        local SegmentID = net_ReadUInt(16)
+        local SegmentsSent = net_ReadUInt(16)
+        local NumSegments = net_ReadUInt(16)
         local Segment = ReadData()
 
-        local SegmentedPacket = Fallback(Globalize.Internal.ActiveSegmentedPackets, SegmentID, {
+        local SegmentedPacket = Fallback(GlobalizeInternal.ActiveSegmentedPackets, SegmentID, {
             data = "",
             len = 0
         })
@@ -320,7 +344,7 @@ net.Receive("Globalize.NetworkChannel", function(len, ply)
             ActiveSegPackets[SegmentID] = nil
 
             local Packet = UncompressPacket(CompressedPacket.data)
-            Globalize.Internal.CallReceiver(Packet.id, Packet.data, len, ply)
+            GlobalizeInternal.CallReceiver(Packet.id, Packet.data, len, ply)
         end
 
         return
@@ -328,35 +352,26 @@ net.Receive("Globalize.NetworkChannel", function(len, ply)
 
     local Packet = UncompressPacket(ReadData())
 
-    Globalize.Internal.CallReceiver(Packet.id, Packet.data, len, ply)
+    GlobalizeInternal.CallReceiver(Packet.id, Packet.data, len, ply)
 end)
 
 --------------------------------------------------------------------------------------------------------------------------------
 
-function Globalize.SetGlobal(VariableID, ...)
+function GlobalizeInternal.SetGlobal(VariableID, ...)
     if VariableID == nil then return end
 
     local Data = {...}
 
-    local GlobalVariables = Globalize.Internal.GlobalVariables
+    local GlobalVariables = GlobalizeInternal.GlobalVariables
 
-    Globalize.Send("GlobalizeInternalGlobalVar", {
-        VariableID = VariableID,
-        Data = Data
-    })
+    GlobalVariables[VariableID] = Data
 
-    table.CopyFromTo(Data, GlobalVariables[VariableID])
+    hook_Run("GlobalizeGlobalChanged", GlobalVariables[VariableID])
+
+    return Data
 end
 
-function Globalize.GetGlobal(VariableID)
-    if VariableID == nil then return end
-
-    local GlobalVariables = Globalize.Internal.GlobalVariables
-
-    return unpack(GlobalVariables[VariableID])
-end
-
-function Globalize.Internal.NetworkGlobal(VariableID, Ply)
+function GlobalizeInternal.NetworkGlobal(VariableID, Ply)
     if VariableID == nil then return end
 
     local Data = {Globalize.GetGlobal(VariableID)}
@@ -365,6 +380,34 @@ function Globalize.Internal.NetworkGlobal(VariableID, Ply)
         VariableID = VariableID,
         Data = Data
     }, Ply)
+
+    return Data
+end
+
+--------------------------------------------------------------------------------------------------------------------------------
+
+function Globalize.SetGlobal(VariableID, ...)
+    if CLIENT then return end
+    if VariableID == nil then return end
+
+    local Data = GlobalizeInternal.SetGlobal(VariableID, ...)
+
+    Globalize.Send("GlobalizeInternalGlobalVar", {
+        VariableID = VariableID,
+        Data = Data
+    })
+
+    return Data
+end
+
+function Globalize.GetGlobal(VariableID)
+    if VariableID == nil then return end
+
+    local GlobalVariables = GlobalizeInternal.GlobalVariables
+
+    if GlobalVariables[VariableID] == nil then return end
+
+    return unpack(GlobalVariables[VariableID])
 end
 
 --------------------------------------------------------------------------------------------------------------------------------
@@ -373,23 +416,23 @@ Globalize.Subscribe("GlobalizeInternalGlobalVar", function(Packet)
     local VariableID = Packet.VariableID
     local Data = Packet.Data
 
-    Globalize.SetGlobal(VariableID, unpack(Data))
+    GlobalizeInternal.SetGlobal(VariableID, unpack(Data))
 end)
 
 --------------------------------------------------------------------------------------------------------------------------------
 
 if CLIENT then
-    hook.Add("InitPostEntity", "GlobalizeAskData", function()
+    hook_Add("InitPostEntity", "GlobalizeAskData", function()
         Globalize.Send("GlobalizeInternalAskForGlobalVar")
     end)
 end
 
 if SERVER then
     Globalize.Subscribe("GlobalizeInternalAskForGlobalVar", function(Packet, len, ply)
-        local GlobalVariables = Globalize.Internal.GlobalVariables
+        local GlobalVariables = GlobalizeInternal.GlobalVariables
 
         for VariableID in pairs(GlobalVariables) do
-            Globalize.Internal.NetworkGlobal(VariableID, ply)
+            GlobalizeInternal.NetworkGlobal(VariableID, ply)
         end
     end)
 end
